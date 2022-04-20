1051290f1d6989f5de2239e5bd11e367b10f4654
caiooliveirah09 <leelros@hotmail.com>
2022-04-20 12:31:33 -0300

    I solved requirement 11

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,6 +1,33 @@
 // Desafio 11
-function generatePhoneNumber() {
-  // seu código aqui
+function generatePhoneNumber(array) {
+  let repeat = {};
+  let tel = ""
+  if (array.length === 11) {
+    tel = "(" + array[0] + array[1] + ")" + " " + array[2] + array[3] + array[4] + array[5] + array[6] + "-" + array[7]
+      + array[8] + array[9] + array[10]
+  } else {
+    return "Array com tamanho incorreto."
+  }
+
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] < 0 | array[i] > 9) {
+      return "não é possível gerar um número de telefone com esses valores"
+    }
+
+    if (repeat[array[i]] !== undefined) {
+      repeat[array[i]] = repeat[array[i]] + 1
+    } else {
+      repeat[array[i]] = 1
+    }
+  }
+
+  for (let i in repeat) {
+    if (repeat[i] >= 3) {
+      return "não é possível gerar um número de telefone com esses valores"
+    }
+  }
+
+  return tel
 }
 
 // Desafio 12
@@ -9,12 +36,23 @@ function triangleCheck() {
 }
 
 // Desafio 13
-function hydrate() {
-  // seu código aqui
+function hydrate(string) {
+  /*let drinks = string.match(/\d+/g)
+  let contador = 0
+  if (drinks.length === 1 && drinks[0] === 1) {
+    return "1 copo de água"
+  } else {
+    for (let i = 0; i < drinks.length; i++) {
+      contador = 
+  }
+  }
+*/
 }
+
 
 module.exports = {
   generatePhoneNumber,
   hydrate,
   triangleCheck,
 };
+
